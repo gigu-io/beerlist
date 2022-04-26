fa2d81fce8bd78da11d8881f6f03a9753087ad6f
Jan Lauber <jan.lauber@protonmail.ch>
2022-04-26 16:38:18 +0200

    user sign in
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
-<template>
+<template lang="">
   <div>
-    <NuxtWelcome />
+    <button class="button">Click Me</button>
   </div>
 </template>
+<script>
+export default {};
+</script>
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
+</style>
